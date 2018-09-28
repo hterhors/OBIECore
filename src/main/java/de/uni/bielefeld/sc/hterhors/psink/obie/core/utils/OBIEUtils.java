9ad543a3c232663e2e3bdca50a97c820a0e4878f
Hendrik <hterhors@techfak.uni-bielefeld.de>
2018-09-28 15:52:33 +0200

    minor changes

--- src/main/java/de/uni/bielefeld/sc/hterhors/psink/obie/core/utils/OBIEUtils.java
+++ src/main/java/de/uni/bielefeld/sc/hterhors/psink/obie/core/utils/OBIEUtils.java
@@ -5,8 +5,8 @@ import de.uni.bielefeld.sc.hterhors.psink.obie.core.ontology.interfaces.IOBIEThi
 public class OBIEUtils {
 
 	/**
-	 * TODO: put in each OBIE class. Override clone() function. 
-	 * Performs a deep clone on the given object using the constructor.
+	 * TODO: put in each OBIE class. Override clone() function. Performs a deep
+	 * clone on the given object using the constructor.
 	 */
 	public static IOBIEThing deepConstructorClone(IOBIEThing scioClass) {
 		if (scioClass == null)
@@ -15,8 +15,8 @@ public class OBIEUtils {
 		try {
 			return scioClass.getClass().getDeclaredConstructor(scioClass.getClass()).newInstance(scioClass);
 		} catch (Exception e) {
-			e.printStackTrace();
 			System.err.println(scioClass);
+			e.printStackTrace();
 			System.exit(1);
 			throw new IllegalArgumentException(e.getMessage());
 		}
